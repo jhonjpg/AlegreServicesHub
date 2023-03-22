da88091171029ced7664afdb09e1b992a9a047b0
jhonjpg <jonathan050315jj@gmail.com>
2023-03-22 12:05:51 -0400

    datos css

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,6 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-  //  desktop
-import "../public/css/home.css";
-import "../public/css/beauty.css";
-import "../public/css/navbarAndFooter.css";
-import "../public/css/carousel.css";
-import "../public/css/supermercado.css";
-
-
-  //  phone
-  import "../public/css/cssphone/homePhone.css";
-  import "../public/css/cssphone/navbarAndFooterPhone.css";
-  import "../public/css/cssphone/carouselPhone.css";
-  import "../public/css/cssphone/supermercadoPhone.css";
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
