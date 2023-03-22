e92eb505c84bf6d984d8bf9d49b0a27ad0d794ff
jhonjpg <jonathan050315jj@gmail.com>
2023-03-22 01:41:34 -0400

    changing css import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,17 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
+  //  desktop
+import "../public/css/home.css";
+import "../public/css/beauty.css";
+import "../public/css/navbarAndFooter.css";
+import "../public/css/carousel.css";
+import "../public/css/supermercado.css";
+  //  phone
+  import "../public/css/cssphone/homePhone.css";
+  import "../public/css/cssphone/navbarAndFooterPhone.css";
+  import "../public/css/cssphone/carouselPhone.css";
+  import "../public/css/cssphone/supermercadoPhone.css";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
